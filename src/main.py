1707b99f02057f5abca21a1c4ab6d738e386a2a2
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-01 20:17:45 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,16 +9,27 @@ from core.db_manager import DatabaseManager
 from src.services.auth_api import AuthAPI
 from src.config import load_config
 
+from core.redis import RedisManager
 
 async def main():
+    redis = RedisManager().get_redis()
+
     config = load_config(".env")
 
     db_manager = DatabaseManager()
     await db_manager.create_tables()
 
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+
     auth_api = AuthAPI(
         secret_key=config.jwt.secret_key,
-        db_manager=db_manager
+        db_manager=db_manager,
+        redis=redis,
+        logger=logger
     )
 
     app = FastAPI()
