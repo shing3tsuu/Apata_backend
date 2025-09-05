d8c40b2a389e9e5c193d5ea112f947af502ed0bf
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-05 23:58:55 +0400

    Update db_manager.py

--- src/core/db_manager.py
+++ src/core/db_manager.py
@@ -1,18 +1,18 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Optional
-from sqlalchemy.pool import NullPool
-import sqlalchemy as db
+from typing import AsyncGenerator
 import logging
-import os
 
-from src.config import load_config
+from src.config import Config
 from .database import Base
 
 class BaseDatabaseManager:
-    def __init__(self, logger: logging.Logger | None = None):
-        self.logger = logger or logging.getLogger(__name__)
-        self.config = load_config(".env")
+    def __init__(
+            self,
+            config: Config
+    ):
+        self.config = config
+
         self.engine: AsyncEngine | None = None
         self.session_factory = None
 
@@ -29,7 +29,6 @@ class BaseDatabaseManager:
                 yield session
                 await session.commit()
             except Exception as e:
-                self.logger.error("Database error: %s", e, exc_info=True)
                 await session.rollback()
                 raise
             finally:
@@ -40,8 +39,6 @@ class BaseDatabaseManager:
             await self.initialize()
 
         async with self.engine.begin() as conn:
-            if isinstance(self.engine.url, str) and "sqlite" in self.engine.url:
-                await conn.execute("PRAGMA foreign_keys=ON")
             await conn.run_sync(Base.metadata.create_all)
 
 
@@ -49,7 +46,6 @@ class DatabaseManager(BaseDatabaseManager):
     async def initialize(self):
         self.engine = create_async_engine(
             url=f"postgresql+asyncpg://{self.config.db.user}:{self.config.db.password}@{self.config.db.host}:{self.config.db.port}/{self.config.db.name}",
-            echo=True,
             pool_size=5,
             max_overflow=10,
             pool_pre_ping=True
