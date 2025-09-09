d82696fc211093501495d9471d317877f3e0ea6e
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-10 01:12:49 +0400

    Update dto.py

--- src/core/dto.py
+++ src/core/dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, validator
 from datetime import datetime
 from typing import List
 
@@ -8,6 +8,13 @@ class UserDTO(BaseModel):
     ecdsa_public_key: str | None = None
     ecdh_public_key: str | None = None
 
+class ContactRequestDTO(BaseModel):
+    id: int
+    sender_id: int
+    receiver_id: int
+    status: str
+    created_at: datetime
+
 class MessageDTO(BaseModel):
     id: int
     sender_id: int
@@ -16,5 +23,3 @@ class MessageDTO(BaseModel):
     timestamp: datetime
     is_delivered: bool
     encryption_version: int
-
-
